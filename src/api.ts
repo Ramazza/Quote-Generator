6e5669693708de17442a91d997bd0b9dc92977ea
PecorinaAmaga01! <01ramazza@gmail.com>
2024-04-05 10:59:35 -0300

    remove api key from api.ts

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,6 @@ const api = axios.create({
     baseURL: 'https://api.api-ninjas.com',
     headers: {
         'X-Api-Key': process.env.REACT_APP_API_KEY,
-        // 'X-Api-Key': 'jXZAY40q4/+da5KSwkalcw==d3NT6J41Zi92wUa4',
         'Content-Type': 'application/json',
     }
 });
